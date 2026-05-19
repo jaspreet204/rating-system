e2f78cda1f7bffef0524509cc6a80db3e3717259
jaspreet <jasspreet4640@gmail.com>
2026-05-19 18:29:33 -0500

    finshed funcatinality in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import StarRating from "./components/StarRating";
+import Dialog from "./components/Dialog";
 
 function App() {
+
+    const [show, setShow] = useState(true);
+    const close = () => {
+        setShow(false);
   return (
-   <></>
-  );
+     <>
+      <section className="main-page">
+        <h1>Star Rating System</h1>
+         <StarRating />
+          {show && ( <Dialog close={close}/>)}
+       </section>
+      </>
+    );
+  }
 }
 
 export default App;
